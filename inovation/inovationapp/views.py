7658f19a02ae254dbeaf6b63a18a2d888b03236c
ryodai <ryodai506@gmail.com>
2020-03-23 23:26:51 +0900

    create signup page

--- inovation/inovationapp/views.py
+++ inovation/inovationapp/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.views.generic import CreateView
+from django.urls import reverse_lazy
 # Create your views here.
+
+def signupfunc(request):
+    if request.method == 'POST':
+        username2 = request.POST['username']
+        password2 = request.POST['password']
+        try:
+            User.objects.get(username=username2)
+            return render(request, 'signup.html', {'error':'このユーザーは登録されています'})
+        except:
+            user = User.objects.create_user(username2, '', password2)
+            return render(request, 'signup.html')
+    return render(request, 'signup.html')
