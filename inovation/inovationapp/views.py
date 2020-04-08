4d34742a7e5047dc31fd9093e97d4bc6639b747d
ryodai <ryodai506@gmail.com>
2020-04-08 17:15:47 +0900

    create idea_list,opinoion and main_list pages

--- inovation/inovationapp/views.py
+++ inovation/inovationapp/views.py
@@ -2,8 +2,11 @@ from django.shortcuts import render,redirect
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
-from django.views.generic import CreateView, TemplateView
+from django.views.generic import CreateView, TemplateView, ListView
 from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin
+from .models import Idea_list,Opinion
+from django.db.models import Q
 # Create your views here.
 
 def signupfunc(request):
@@ -27,6 +30,64 @@ def signupfunc(request):
 
 class Home(TemplateView):
     template_name = "home.html"
+
+class Ideas_list(LoginRequiredMixin, ListView):
+    template_name = 'ideas_list.html'
+    model =  Idea_list
+    def get_queryset(self):
+        q_word = self.request.GET.get('query')
+
+        if q_word:
+            object_list = Idea_list.objects.filter(
+                Q(genre__icontains=q_word))
+        else:
+            object_list = Idea_list.objects.all()
+        return object_list
+
+@login_required
+def opinion(request,pk):
+    idea = Idea_list.objects.get(pk=pk).genre
+    if request.method == "POST":
+        author2 = request.POST['author']
+        title2 = request.POST['title']
+        content2 = request.POST['content']
+        Opinion.objects.create(author = author2,title=title2,content = content2,idea_list_id = pk)
+        return redirect('main_list',pk=pk)
+    return render(request, 'opinion.html',{'object':idea})
+
+@login_required
+def main_list(request,pk):
+    d = {
+        'main_idea':Opinion.objects.filter(idea_list_id=pk)
+    }
+    return render(request,"main_list.html",d)
+
+@login_required        
+def goodfunc(request, pk):
+    post =  Opinion.objects.get(pk=pk)
+    pk2 = post.idea_list_id
+    post2 = request.user.get_username()
+    if post2 in post.goodcheck:
+        return redirect('main_list',pk=pk2)
+    else:
+        post.good += 1
+        post.goodcheck = post.goodcheck + ' ' + post2
+        post.save()
+        return redirect('main_list',pk=pk2)
+
+def branchfunc(request, pk):
+    user = request.user.get_username()
+    post = Opinion.objects.filter(idea_list_id=pk).filter(author=user)
+    print(type(post))
+    if not post:
+        return redirect('opinion',pk=pk)
+    else:
+        return redirect('main_list',pk=pk)
+    
+
+
+
+
 
 
 
